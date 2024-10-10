48d44bd86823a02a6d05a150ed8a4d12e44e2a97
unknown <22520882@gm.uit.edu.vn>
2024-10-10 19:11:43 +0700

    second commit

---
+++ WebApplication1/Controllers/CatalogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext("Data Source=DESKTOP-A613PHH;Initial Catalog=QuanLySanPham;User ID=sa;Password=123;Trust Server Certificate=True");
+            List<Catalog> dsCatalog = context.Catalogs.ToList();
+            return View(dsCatalog);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext("Data Source=DESKTOP-A613PHH;Initial Catalog=QuanLySanPham;User ID=sa;Password=123;Trust Server Certificate=True");
+            List<Product> dsProduct = context.Products.ToList();
+            return View(dsProduct);
+        }
+    }
+}
